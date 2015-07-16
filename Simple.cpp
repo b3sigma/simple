5fe2ece1edc8af0f261114bfbb91dd382e206092
b3sigma <b3sigma@gmail.com>
2015-07-16 14:57:56 -0700

    Add a trivial simple proj for each and quick startup.

---
+++ Simple.cpp
@@ -0,0 +1,15 @@
+#include <memory>
+#include <stdio.h>
+#include <list>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <hash_map>
+#include <algorithm>
+
+int main(int argc, char* argv[])
+{
+
+  return 0;
+}
+
